7aab5b838eede31040822bf32d0b03336f836902
Anastasia Migaeva <pandora1983@yandex.ru>
2023-05-15 16:12:01 +0300

    Webpack. Введение в арх-ру. Code splitting. Css Modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import {Routes, Route, Link} from 'react-router-dom';
-import {Suspense} from 'react';
-import './index.scss'
+import {Suspense, useContext, useState} from 'react';
+import './styles/index.scss'
 import AboutPage from './pages/AboutPage/AboutPage';
 import MainPage from './pages/MainPage/MainPage';
 import {AboutPageAsync} from './pages/AboutPage/AboutPage.async';
 import {MainPageAsync} from './pages/MainPage/MainPage.async';
+import {Theme, ThemeContext} from './theme/ThemeContext';
+import {useTheme} from './theme/useTheme';
 
 const App = () => {
+  const {theme, toggleTheme} = useTheme();
+
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
       <ul>
         <li>
           <Link to="/about">About</Link>
@@ -17,6 +21,8 @@ const App = () => {
           <Link to="/">Main</Link>
         </li>
       </ul>
+
+      <button onClick={toggleTheme}>Toggle theme</button>
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
           <Route path="/about" element={<AboutPageAsync />} />
